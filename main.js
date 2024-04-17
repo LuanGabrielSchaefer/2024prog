1c12527c8dfdf4ea133c8bfa6e98f765629adfa5
LuanGabrielSchaefer <127956901+LuanGabrielSchaefer@users.noreply.github.com>
2024-04-17 08:30:45 -0300

    arquivo

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const botoes = document.querySelectorA11(".botao");
-const textos = document.querySelectorA11("aba-conteudo");
+const botoes = document.querySelectorAll(".botao");
+const textos = documet.querySelectorAll(".aba-conteudo");
+
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function () {
 
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
-            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
@@ -13,31 +14,40 @@ for(let i=0;i <botoes.length;i++){
     }
 }
 
-const contadores = document.querySelectorA11(".contador");
-const tempoObjetivo1 = new date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new date("2023-02-01T00:00:00");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
 
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
-
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
-
-for(let i=0; i<contadores.length;i++){
-    
-}
-
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = MAth.floor (tempoFinal/1000);
+    let segundos = Math.floor(tempoFinal/1000);
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
-    let dias = Math.floor(horas/24);
-
-    segundos %=60;
-    minutos %=60;
-
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    let dias = Math.floor(horas/20);
+    
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    } else {
+        return "Prazo Finalizado";
+    }
+    
 }
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+function comecaCronometro(){
+
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+//comecaCronometro();
